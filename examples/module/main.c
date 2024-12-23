ef51a29e847c6110954f791658484b663c5a458c
Patryk Kubiak <patryk.kubiak90@gmail.com>
2024-12-23 23:18:07 +0100

    Added workaround for global pointers

--- examples/module/main.c
+++ examples/module/main.c
@@ -3,9 +3,14 @@
 
 extern Dmod_ModuleHeader_t ModuleHeader;
 
+const char* HelloWorld DMOD_GLOBAL_POINTER = "Some global pointer";
+
 int main(int argc, char** argv)
 {
     Dmod_Printf("Hello from module: %s\n", ModuleHeader.Name);
     Dmod_Printf("Version: %s\n", ModuleHeader.Version);
+    Dmod_Printf("Author: %s\n", ModuleHeader.Author);
+    Dmod_Printf("Arch: %s\n", ModuleHeader.Arch);
+    Dmod_Printf("Hello World: %s\n", HelloWorld);
     return 0;
 }
--- inc/dmod_defs.h
+++ inc/dmod_defs.h
@@ -11,6 +11,7 @@
 #define DMOD_WEAK_SYMBOL		__attribute__((weak))
 #define DMOD_SECTION( NAME )	__attribute__((section(NAME)))
 #define DMOD_USED                        __attribute__((used))
+#define DMOD_GLOBAL_POINTER             DMOD_SECTION(".got")
 
 #ifndef DMOD_STACK_ALIGNMENT
 #   define DMOD_STACK_ALIGNMENT		16
