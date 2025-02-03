f4a491b966c55e912c15fcfb1fe049eac338bcb2
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-02-03 16:52:05 +0100

    Remove DMOD_UNIT_TESTS conditional definitions and relocate input/output variables to unit test file

--- tests/ut_main.cpp
+++ tests/ut_main.cpp
@@ -1,5 +1,15 @@
 #include <gtest/gtest.h>
 
+extern "C"
+{
+void* __dmod_inputs_start = 0;
+void* __dmod_inputs_end = 0;
+void* __dmod_inputs_size = 0;
+void* __dmod_outputs_start = 0;
+void* __dmod_outputs_end = 0;
+void* __dmod_outputs_size = 0;
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
