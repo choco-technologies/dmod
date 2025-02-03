9a731512d2d5fc1e428327f9511b3072a262e191
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-02-03 21:42:06 +0100

    Increase minimum code coverage to 48%; add Dmod_Mgr unit tests

--- tests/ut_main.cpp
+++ tests/ut_main.cpp
@@ -1,13 +1,17 @@
 #include <gtest/gtest.h>
-
+#include <dmod.h>
 extern "C"
 {
-void* __dmod_inputs_start = 0;
-void* __dmod_inputs_end = 0;
-void* __dmod_inputs_size = 0;
-void* __dmod_outputs_start = 0;
-void* __dmod_outputs_end = 0;
-void* __dmod_outputs_size = 0;
+void DMOD_WEAK_SYMBOL Dmod_TestApi1() {}
+void* __dmod_inputs_start DMOD_SECTION(".dmod.inputs.test")  = 0;
+volatile Dmod_ApiRegistration_t __dmod_inputs[] DMOD_SECTION(".dmod.inputs.test") DMOD_USED = {
+    { .Function = (void*)Dmod_TestApi1, .Signature = DMOD_MAKE_SIGNATURE(Dmod, 1.0, TestApi1) },
+};
+void* __dmod_inputs_end DMOD_SECTION(".dmod.inputs.test")= 0;
+void* __dmod_inputs_size DMOD_SECTION(".dmod.inputs.test")= 0;
+void* __dmod_outputs_start DMOD_SECTION(".dmod.outputs.test")= 0;
+void* __dmod_outputs_end DMOD_SECTION(".dmod.outputs.test")= 0;
+void* __dmod_outputs_size DMOD_SECTION(".dmod.outputs.test")= 0;
 }
 
 int main(int argc, char **argv) {
