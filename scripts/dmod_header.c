1aa76ed6f934a8f24cebaa01aa571b7732916937
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-02-26 23:22:58 +0100

    Add Dmod_Context_GetModuleType function and corresponding tests; update pipeline script

--- scripts/dmod_header.c
+++ scripts/dmod_header.c
@@ -36,66 +36,6 @@ extern int  DMOD_WEAK_SYMBOL dmod_signal( int SignalNumber );
 extern Dmod_License_t DMOD_WEAK_SYMBOL License;
 extern void* __footer_start;
 
-typedef void (*func_ptr)(void);
-extern func_ptr __init_array_start[];
-extern func_ptr __init_array_end[];
-extern func_ptr __fini_array_start[];
-extern func_ptr __fini_array_end[];
-
-/**
- * @brief Call functions from the init array
- * 
- * This function calls all the functions from the init array.
- */
-static int _init(const Dmod_Config_t *Config) 
-{
-    Dmod_Printf("Initializing module: %s\n", DMOD_MODULE_NAME);
-    for (func_ptr *func = __init_array_start; func < __init_array_end; ++func) 
-    {
-        if (*func)
-        {
-            (*func)();
-        }
-    }
-
-    volatile void* init = dmod_init;
-    Dmod_Printf("Constructor step has been finished. dmod_init: %p\n", init);
-
-    int result = 0;
-    if(init)
-    {
-        result = dmod_init(Config);
-    }
-    return result;
-}
-
-/**
- * @brief Call functions from the fini array
- * 
- * This function calls all the functions from the fini array.
- */
-static int _fini(void)
-{
-    Dmod_Printf("Deinitializing module: %s\n", DMOD_MODULE_NAME);
-    for (func_ptr *func = __fini_array_end - 1; func >= __fini_array_start; --func) 
-    {
-        if (*func) 
-        {
-            (*func)();
-        }
-    }
-
-    volatile void* deinit = dmod_deinit;
-    Dmod_Printf("Destructor step has been finished. dmod_deinit: %p\n", deinit);
-
-    int result = 0;
-    if(deinit)
-    {
-        result = dmod_deinit();
-    }
-    return result;
-}
-
 volatile const Dmod_ModuleHeader_t ModuleHeader DMOD_SECTION(".header") DMOD_USED = 
 {
     .Signature          = DMOD_HEADER_SIGNATURE,
@@ -106,9 +46,9 @@ volatile const Dmod_ModuleHeader_t ModuleHeader DMOD_SECTION(".header") DMOD_USE
     .Author             = DMOD_AUTHOR_NAME,
     .Version            = DMOD_MODULE_VERSION,
     .Preinit            = dmod_preinit,
-    .Init               = _init,
+    .Init               = dmod_init,
     .Main               = main,
-    .Deinit             = _fini,
+    .Deinit             = dmod_deinit,
     .Signal             = dmod_signal,
     .RequiredStackSize  = DMOD_STACK_SIZE,
     .Priority           = DMOD_PRIORITY,
