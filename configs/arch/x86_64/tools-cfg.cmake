2b30c508cc50c8d31da8b7aed0622ec78515ecb9
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-16 13:54:51 +0100

    Refactor toolchain configuration in tools-cfg.cmake to improve compiler detection and set default commands for make, mkdir, and rm

--- configs/arch/x86_64/tools-cfg.cmake
+++ configs/arch/x86_64/tools-cfg.cmake
@@ -23,15 +23,70 @@ endif()
 if(NOT DEFINED CROSS_COMPILE)
 	set(CROSS_COMPILE "")
 endif()
-set(CMAKE_C_COMPILER "${CROSS_COMPILE}gcc")
-set(CMAKE_CXX_COMPILER "${CROSS_COMPILE}g++")
-set(CMAKE_LINKER "${CROSS_COMPILE}ld")
-set(CMAKE_OBJDUMP "${CROSS_COMPILE}objdump")
-set(CMAKE_OBJCOPY "${CROSS_COMPILE}objcopy")
-set(CMAKE_AR "${CROSS_COMPILE}ar")
-set(MAKE "make")
-set(MKDIR "mkdir")
-set(RM "rm")
+
+
+
+#
+#	Toolchain configuration
+#
+if(NOT DEFINED CROSS_COMPILE)
+	set(CROSS_COMPILE "")
+endif()
+
+find_program(GCC ${CROSS_COMPILE}gcc)
+if(NOT GCC)
+    message(FATAL_ERROR "GCC compiler not found")
+endif()
+
+find_program(GXX ${CROSS_COMPILE}g++)
+if(NOT GXX)
+    message(FATAL_ERROR "G++ compiler not found")
+endif()
+
+find_program(LD ${CROSS_COMPILE}ld)
+if(NOT LD)
+    message(FATAL_ERROR "Linker not found")
+endif()
+
+find_program(OBJDUMP ${CROSS_COMPILE}objdump)
+if(NOT OBJDUMP)
+    message(FATAL_ERROR "objdump not found")
+endif()
+
+find_program(OBJCOPY ${CROSS_COMPILE}objcopy)
+if(NOT OBJCOPY)
+    message(FATAL_ERROR "objcopy not found")
+endif()
+
+find_program(AR ${CROSS_COMPILE}ar)
+if(NOT AR)
+    message(FATAL_ERROR "ar not found")
+endif()
+
+find_program(SIZE ${CROSS_COMPILE}size)
+if(NOT SIZE)
+    message(FATAL_ERROR "size not found")
+endif()
+
+find_program(GDB ${CROSS_COMPILE}gdb)
+if(NOT GDB)
+    message(FATAL_ERROR "GDB not found")
+endif()
+
+# ==============================================================================
+#                         CMake Configuration
+# ==============================================================================
+set(CMAKE_C_COMPILER "${GCC}" CACHE STRING "C compiler")
+set(CMAKE_CXX_COMPILER "${GXX}" CACHE STRING "C++ compiler")
+set(CMAKE_LINKER "${LD}" CACHE STRING "Linker")
+set(CMAKE_OBJDUMP "${OBJDUMP}" CACHE STRING "Objdump")
+set(CMAKE_OBJCOPY "${OBJCOPY}" CACHE STRING "Objcopy")
+set(CMAKE_SIZE "${SIZE}" CACHE STRING "Size")
+set(CMAKE_AR "${AR}" CACHE STRING "Archiver")
+set(CMAKE_GDB "${GDB}" CACHE STRING "GDB")
+set(MAKE make CACHE STRING "Make")
+set(MKDIR mkdir CACHE STRING "Mkdir")
+set(RM rm CACHE STRING "Rm")
 set(CMAKE_C_FLAGS "-Wall -std=c11 ${CPUCONFIG_CFLAGS}")
 set(CMAKE_CXX_FLAGS "-Wall -std=c++17 ${CPUCONFIG_CXXFLAGS}")
 set(CMAKE_LFLAGS "${CPUCONFIG_LDFLAGS}")
