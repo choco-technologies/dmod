c099266753e31feaf2a441429a210a960483a7ae
Patryk Kubiak <patryk.kubiak90@gmail.com>
2025-11-16 14:18:09 +0100

    Add support for commit hash retrieval in Dockerfile and version files

--- dmod-info.cmake
+++ dmod-info.cmake
@@ -3,3 +3,11 @@
 # -----------------------------------------------------------------------------
 set(dmod_VERSION_MAJOR 0)
 set(dmod_VERSION_MINOR 1)
+
+execute_process(
+    COMMAND git rev-parse --short HEAD
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    OUTPUT_VARIABLE DMOD_COMMIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+)
