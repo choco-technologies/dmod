8c7b94fd392b3a01edaa1e09b4c4f845e48f650f
Patryk Kubiak <patryk.kubiak90@gmail.com>
2026-05-28 11:12:48 +0200

    Fixed interrupt handling

--- inc/dmod_defs.h
+++ inc/dmod_defs.h
@@ -253,7 +253,7 @@ extern "C" {
  */
 #define DMOD_IRQ_HANDLER( IRQ_NUMBER )        \
         static void DMOD_IRQ_MAKE_HANDLER_NAME(IRQ_NUMBER)(void);\
-        static Dmod_ApiRegistration_t DMOD_IRQ_MAKE_REG_NAME(IRQ_NUMBER) DMOD_USED_SECTION(".inputs") = \
+        volatile const Dmod_ApiRegistration_t DMOD_IRQ_MAKE_REG_NAME(IRQ_NUMBER) DMOD_USED_SECTION(".dmod.inputs") = \
         { \
             .Function = (void*)DMOD_IRQ_MAKE_HANDLER_NAME(IRQ_NUMBER), \
             .Signature = DMOD_MAKE_IRQ_SIGNATURE(IRQ_NUMBER) \
