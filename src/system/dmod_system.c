3806b6b9a4649d16549f4f6ef70357182097a541
Patryk Kubiak <patryk.kubiak90@gmail.com>
2026-09-19 09:42:46 +0200

    Removed annoying warning

--- src/system/dmod_system.c
+++ src/system/dmod_system.c
@@ -1111,7 +1111,6 @@ bool Dmod_Enable( Dmod_Context_t* Context, bool Force, const Dmod_Config_t* Conf
 
     if( Context->Enabled && !Force )
     {
-        DMOD_LOG_WARN("Module already enabled\n");
         Dmod_Mutex_Unlock(Context->Mutex);
         return true;
     }
